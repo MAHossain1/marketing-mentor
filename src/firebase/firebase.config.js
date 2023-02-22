9d6ca8139ed3fcf0b8bd552deb6a618c6f3c6fd9
Md. Arman Hossain <mahpro110@gmail.com>
2023-02-22 17:40:34 +0600

    project start

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB38aZvcTA-P7_Fa8tiKL3mT4j_ijvmu9Q",
+  authDomain: "marketing-mentor.firebaseapp.com",
+  projectId: "marketing-mentor",
+  storageBucket: "marketing-mentor.appspot.com",
+  messagingSenderId: "483519768907",
+  appId: "1:483519768907:web:3f433085fd2f051c2da0ce",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
