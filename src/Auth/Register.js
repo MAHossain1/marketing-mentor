d7502a7f93a7d8bcfba761360d57f7ba0f08f368
Md. Arman Hossain <mahpro110@gmail.com>
2023-02-23 17:37:07 +0600

    user login and register UI done

--- src/Auth/Register.js
+++ src/Auth/Register.js
@@ -1,9 +1,86 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h2>Register here</h2>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register now!</h1>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Full Name</span>
+              </label>
+              <input
+                type="text"
+                name="fullName"
+                placeholder="Enter Your Full Name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">photo URL</span>
+              </label>
+              <input
+                type="text"
+                name="photoURl"
+                placeholder="Your photo link"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+              <label className="label">
+                Already have an account?
+                <Link to="/login" className="label-text-alt link link-hover">
+                  please Login
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+          </form>
+          {/* <div className="form-control m-6 mt-0">
+          <button className="btn btn-outline btn-success">
+            Login with Google
+          </button>
+          <button className="btn btn-outline btn-success mt-2">
+            Login with Github
+          </button>
+        </div> */}
+        </div>
+      </div>
     </div>
   );
 };
